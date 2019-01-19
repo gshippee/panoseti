2db64a6f1428203dc68007b22b647c009b910a32
gshippee <gshippee@berkeley.edu>
2019-01-18 19:48:54 -0600

    added code in c to simulate packets being sent from multiple modules

--- acq_data/read_data.py
+++
@@ -1,9 +0,0 @@
-from struct import *
-import sys
-
-with open('data_1547763551.bin', mode = 'rb') as file:
-    while True:
-        header = file.read(16)
-        data = file.read(512)
-        packet = unpack("LHBB2l", header)
-        print(packet)
--- udp_client.py
+++ udp_client.py
@@ -13,11 +13,12 @@ def UtcNow():
 
 
 def MakePacket(module, quad, packet_no):
-    header = pack('2H4s2l', 1, packet_no%65536, str(module+.1*quad).zfill(4), UtcNow()[0], UtcNow()[1])
+    header = pack('LHBB2l', packet_no, 1, module, quad, UtcNow()[0], UtcNow()[1])
+    print(unpack('LHBB2l',header))
+    #time.sleep(.001)
     data = pack('256h', *np.random.poisson(5, 256))
     packet = header+data
     return packet
-    #print(unpack('2h4s2l256h',packet))
     #print('')
     #print('')
     #print('')
@@ -29,23 +30,24 @@ def MakePacket_helper(args):
 
 def Quabo(module):
     global sockets
-    quads = [1,2,3,4]
+    #quads = [1,2,3,4]
+    quads = [1]
     i = 0
     socket = sockets[module]
     while(True):
         job_args = [(module,quad, i) for j,quad in enumerate(quads)] 
         socket[0].sendto(MakePacket(*job_args[0]), (localIP, localPort))
-        socket[1].sendto(MakePacket(*job_args[1]), (localIP, localPort))
-        socket[2].sendto(MakePacket(*job_args[2]), (localIP, localPort))
-        socket[3].sendto(MakePacket(*job_args[3]), (localIP, localPort))
-        #time.sleep(.001)
+        #socket[1].sendto(MakePacket(*job_args[1]), (localIP, localPort))
+        #socket[2].sendto(MakePacket(*job_args[2]), (localIP, localPort))
+        #socket[3].sendto(MakePacket(*job_args[3]), (localIP, localPort))
+        #time.sleep(.005)
         i+=1
 
 no_modules = 1
 sockets = []
 quabo_sockets = []
 for i in range(no_modules):
-    for j in range(4):
+    for j in range(1):
         vars()['sock'+str(j)] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         quabo_sockets.append(vars()['sock'+str(j)])
     #vars()['sock'+str(i)] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -53,8 +55,10 @@ for i in range(no_modules):
     sockets.append(quabo_sockets)
 modules = np.arange(0,no_modules,1)
 
-def main():
 
+
+
+def main():
     q = Pool(no_modules)
     start_time = datetime.datetime.utcnow()
     q.map(Quabo,modules)
@@ -64,15 +68,15 @@ def main():
 
 
 sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#localIP = "127.0.0.1"
-localIP = '100.64.4.188'
-localPort = 6789
+localIP = "127.0.0.1"
+#localIP = '100.64.4.188'
+localPort = 10001
 bufferSize = 1024
 Message = "Hello"
 Message= 'f1a525da11f6'.decode('hex')
 Message1 = "Goodbye"
 
-MakePacket(1,1,0)
+#MakePacket(1,1,0)
 if __name__ == '__main__':
     main()
 
